79ab8f71c8ef10b70e830dec1474487102e7ab33
Vjchavan <vijaychavan582000@gmail.com>
2024-03-26 20:42:40 +0530

    Upload project structure

---
+++ src/main/java/com/project/quizzywhiz/QuizzywhizApplication.java
@@ -0,0 +1,13 @@
+package com.project.quizzywhiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzywhizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzywhizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/quizzywhiz/QuizzywhizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quizzywhiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzywhizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
